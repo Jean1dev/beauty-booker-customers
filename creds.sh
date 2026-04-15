86ff42c9cee76882968953e34642d91570dd2afc
Jeanluca <jeanlucafp@gmail.com>
2026-04-15 08:27:27 -0300

    Add package identifier to app.json and create eas.json for build configuration

---
+++ creds.sh
@@ -0,0 +1,10 @@
+npm install -g eas-cli
+
+eas credentials
+eas build --platform android --profile production
+eas build --platform android --profile preview
+
+eas build --platform ios --profile production
+eas build --platform ios --profile validation
+
+eas submit
